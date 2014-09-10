8f8e959bec6352c91d74b7929c8fb924a503f242
cuki <mauricio@krebs.com.br>
2014-09-10 09:41:14 -0300

    lendo de acordo com a entrada

--- toroide.c
+++ toroide.c
@@ -13,13 +13,14 @@
 #USE delay(clock=16MHz)
 #USE rs232(xmit=PIN_C6, BAUD=9600)
 
+#define padrao 100
+
 long reed;
 long reedAux;
 
-long freq;
-long freqAux;
+float freq;
 
-int latencia = 2;
+int latencia = padrao;
 
 float aux;
 
@@ -28,25 +29,27 @@ int main(void) {
 	SETUP_ADC(ADC_CLOCK_INTERNAL);
 	SETUP_ADC_PORTS(AN0_AN1_AN3);
 
+	if (!latencia)
+		latencia = padrao;
+
 	while (TRUE) {
 
 		set_adc_channel(0);
 		delay_ms(latencia);
-		freq = read_adc();
+		reed = read_adc();
 
-		if (freq != freqAux) {
-			freqAux = freq;
-			printf("AN0: %f\n\r", (float) freq / 10.24);
+		if (reedAux != reed) {
+			reedAux = reed;
+			freq = (float) (59 * reed / 1024) + 1;
+			latencia = (int) 1 / freq * 500;
+			printf("Lat: %ums Freq: %fHz\n\r", latencia, freq);
 		}
 
 		set_adc_channel(3);
 		delay_ms(latencia);
 		reed = read_adc();
-
-		if (reed != reedAux) {
-			reedAux = reed;
-			printf("AN3: %f\n\r", (float) reed / 10.24);
-		}
+		reedAux = reed;
+		printf("Tensao: %f\n\r", (float) 5 * reed / 1024);
 	}
 	return 0;
 }
