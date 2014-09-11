625281858fa9624f81ac8e5e1f6cc89e4312af38
cuki <mauricio@krebs.com.br>
2014-09-11 14:18:45 -0300

    com interrupcao

--- toroide.c
+++ toroide.c
@@ -13,40 +13,56 @@
 #USE delay(clock=16MHz)
 #USE rs232(xmit=PIN_C6, BAUD=9600)
 
-#define padrao 100
+#define botStart	PIN_B0
+#define tmr0reg		0
+#define bufferSize	600
 
-long reed;
-long reedAux;
+short ctrl;
 
-float freq;
+long vetor[bufferSize];
+long cont;
 
-int latencia = padrao;
-
-float aux;
+#INT_TIMER0
+void isr_timer0() {
+	clear_interrupt(INT_TIMER0);
+	set_timer0(tmr0reg);
+	vetor[cont++] = read_adc();
+	if (cont >= bufferSize)
+		disable_interrupts(INT_TIMER0);
+}
 
 int main(void) {
 
 	SETUP_ADC(ADC_CLOCK_INTERNAL);
-	SETUP_ADC_PORTS(AN0_AN1_AN3);
+	SETUP_ADC_PORTS(AN0);
+	set_adc_channel(0);
 
-	if (!latencia)
-		latencia = padrao;
+	set_timer0(tmr0reg);
+	setup_timer_0(T0_INTERNAL | T0_8_BIT);
+
+	clear_interrupt(INT_TIMER0);
+//	enable_interrupts(INT_TIMER0);
+//	enable_interrupts(GLOBAL);
 
 	while (TRUE) {
 
-		set_adc_channel(0);
-		delay_ms(latencia);
-		reed = read_adc();
-
-		if (reedAux != reed) {
-			reedAux = reed;
-			freq = (float) (59 * reed / 1024) + 1;
-			latencia = (int) 1 / freq * 500;
+		if (!input(botStart)) {
+			delay_ms(100);
+			if (!input(botStart) && !ctrl) {
+				ctrl = TRUE;
+				enable_interrupts(INT_TIMER0);
+				enable_interrupts(GLOBAL);
+			}
 		}
 
-		set_adc_channel(3);
-		delay_ms(latencia);
-		printf("%lu\n\r", read_adc());
+		if (cont >= bufferSize) {
+//			delay_ms(1000);
+			for (cont = 0; cont < bufferSize; ++cont) {
+				printf("%lu\n\r", vetor[cont]);
+			}
+			return 0;
+		}
+
 	}
 	return 0;
 }
