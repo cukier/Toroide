7e829e1c21d9c33c1720c752abdd674ec2eea40d
cuki <mauricio@krebs.com.br>
2014-09-12 09:39:27 -0300

    botado

--- toroide.c
+++ toroide.c
@@ -14,35 +14,17 @@
 #USE rs232(xmit=PIN_C6, BAUD=9600)
 
 #define botStart	PIN_B0
-#define tmr0reg		0
-#define bufferSize	600
+#define bufferSize	750
 
 short ctrl;
 
 long vetor[bufferSize];
 long cont;
-
-#INT_TIMER0
-void isr_timer0() {
-	clear_interrupt(INT_TIMER0);
-	set_timer0(tmr0reg);
-	vetor[cont++] = read_adc();
-	if (cont >= bufferSize)
-		disable_interrupts(INT_TIMER0);
-}
-
 int main(void) {
 
 	SETUP_ADC(ADC_CLOCK_INTERNAL);
 	SETUP_ADC_PORTS(AN0);
 	set_adc_channel(0);
-
-	set_timer0(tmr0reg);
-	setup_timer_0(T0_INTERNAL | T0_8_BIT);
-
-	clear_interrupt(INT_TIMER0);
-//	enable_interrupts(INT_TIMER0);
-//	enable_interrupts(GLOBAL);
 
 	while (TRUE) {
 
@@ -50,19 +32,19 @@ int main(void) {
 			delay_ms(100);
 			if (!input(botStart) && !ctrl) {
 				ctrl = TRUE;
-				enable_interrupts(INT_TIMER0);
-				enable_interrupts(GLOBAL);
-			}
+				cont = 0;
+				while (cont < bufferSize) {
+					vetor[cont++] = read_adc();
+					while (!adc_done())
+						;
+				}
+				for (cont = 0; cont < bufferSize; ++cont) {
+					printf("%lu\n\r", vetor[cont]);
+				}
+				cont = 0;
+			} else if (ctrl)
+				ctrl = FALSE;
 		}
-
-		if (cont >= bufferSize) {
-//			delay_ms(1000);
-			for (cont = 0; cont < bufferSize; ++cont) {
-				printf("%lu\n\r", vetor[cont]);
-			}
-			return 0;
-		}
-
 	}
 	return 0;
 }
